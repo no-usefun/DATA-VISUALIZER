00b50076d6f0990fd2b22abecd6d279fc08c7227
Harsh Agarwal <itsharsh636@gmail.com>
2026-02-25 01:41:51 +0530

    Data visualizer

---
+++ FullStackVisualizer/src/main/java/com/project/FullStackVisualizer/FullStackVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.project.FullStackVisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackVisualizerApplication.class, args);
+	}
+
+}
